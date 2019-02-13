0180908ae8ebdb1aa8b89540a668c3d278f50058
Nurlan Mirzayev <Nurlan_Mirzayev@epam.com>
2019-02-13 17:54:39 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        "node": true
+    },
+    "rules": {
+        "no-console": "off",
+        "global-require": "off",
+        "import/no-dynamic-require": "off",
+        "linebreak-style": "off"
+    }
+};
