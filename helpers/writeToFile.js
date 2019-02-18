a71004ba08759a74751ce9e1997472a0593a81ca
Nurlan Mirzayev <Nurlan_Mirzayev@epam.com>
2019-02-18 17:02:08 +0100

    hw4 done

--- helpers/writeToFile.js
+++ helpers/writeToFile.js
@@ -1,3 +1,20 @@
-export default (data) => {
-  return data;
+import fs from 'fs';
+import products from '../data/products.json';
+
+export default (data, res, next) => {
+  const writable = fs.createWriteStream('./data/products.json');
+
+  writable.write(JSON.stringify([
+    ...products,
+    {
+      ...data,
+      id: products[products.length - 1].id + 1,
+    },
+  ]));
+
+  writable.on('error', (error) => {
+    next(error);
+  });
+
+  writable.end(() => res.json(products));
 };
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,4 @@
-import products from '../data/products';
+import products from '../data/products.json';
 import writeToFile from '../helpers/writeToFile';
 
 export default (router) => {
@@ -7,13 +7,7 @@ export default (router) => {
       res.json(products);
     })
     .post((req, res, next) => {
-      console.log('====================================');
-      console.log(req.body);
-      console.log('====================================');
-      res.json(writeToFile(req.body));
-      // } catch (error) {
-      //   next(`We weren't able to save ${error}`);
-      // }
+      writeToFile(req.body, res, next);
     });
 
   router.get('/products/:id', (req, res, next) => {
@@ -21,6 +15,16 @@ export default (router) => {
 
     if (product) {
       res.json(product);
+    } else {
+      next(new Error(`Can't find product with id:${req.params.id}`));
+    }
+  });
+
+  router.get('/products/:id/options', (req, res, next) => {
+    const product = products.find(({ id }) => id === +req.params.id);
+
+    if (product) {
+      res.json(product.options);
     } else {
       next(new Error(`Can't find product with id:${req.params.id}`));
     }
