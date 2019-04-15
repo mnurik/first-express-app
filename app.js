cb4f37618cf99b6c6c52f3560d8919038c894be5
Nurlan Mirzayev <Nurlan_Mirzayev@epam.com>
2019-04-15 16:32:01 +0200

    started use graphql

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import { buildSchema } from 'graphql';
+import graphqlHTTP from 'express-graphql';
 import parsers from './middlewares/parsers';
 import passport from './middlewares/passport';
-import { products, auth, users, cities } from './routes/index';
-
+import {
+  products,
+  auth,
+  users,
+  cities,
+} from './routes/index';
 import './middlewares/localStrategy';
 
 const app = express();
@@ -10,10 +16,25 @@ const app = express();
 parsers(app);
 passport(app);
 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => 'Hello world!',
+};
+
 app.use('/api/products', products);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/cities', cities);
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
